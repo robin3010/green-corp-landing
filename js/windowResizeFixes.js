7fce190e5aa05a90752b7dbf5267e865b5048c74
robin3010 <robin3010@gmail.com>
2022-11-23 23:47:07 +0300

    intendation => 2 spaces, index.html formatted

--- js/windowResizeFixes.js
+++ js/windowResizeFixes.js
@@ -1,17 +1,15 @@
-const body = document.querySelector("body");
-    const setBodyMarginR = () => {
-      body.style["margin-right"] = -(body.offsetWidth - body.clientWidth) + "px";
-    };
-    window.addEventListener("resize", () => {
-      let delay = 250, throttled = false;
-      if (!throttled) {
-        setBodyMarginR();
-        canvas = new CanvasBG("orb-canvas");
-        canvas.start();
-        throttled = true;
-        setTimeout(function() {
-          throttled = false;
-        }, delay);
-      }
-    });
+const body = document.body;
+const setBodyMarginR = () => {
+  body.style["margin-right"] = -(body.offsetWidth - body.clientWidth) + "px";
+};
+window.addEventListener("resize", () => {
+  let delay = 250, throttled = false;
+  if (!throttled) {
     setBodyMarginR();
+    throttled = true;
+    setTimeout(function () {
+      throttled = false;
+    }, delay);
+  }
+});
+setBodyMarginR();
